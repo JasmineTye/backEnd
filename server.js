b17697b8fbab7e10ed91086b50536cd925e7865c
ybizam <ybizam.20@ichat.sp.edu.sg>
2021-07-27 14:56:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,24 @@ console.log("---------------------------------------");
 // imports
 //----------------------------------------------
 const app = require('./controller/app');
-const publicIp = require('public-ip');
-
+// const publicIp = require('public-ip');
+const port = process.env.PORT || 4201;
 //----------------------------------------------
 // configurations
 //----------------------------------------------
-(async () => {
-	const hostname = await publicIp.v4();
-	const port = 3000;
+// (async () => {
+// 	const hostname = await publicIp.v4();
+// 	const port = 3000;
 
-    //----------------------------------------------
-    // main
-    //----------------------------------------------
-    // start the server and start listening for incoming requests
-    app.listen(port, hostname, () => {
-        console.log(`Server started and accessible via http://${hostname}:${port}/`);
-    });
-})();
+//     //----------------------------------------------
+//     // main
+//     //----------------------------------------------
+//     // start the server and start listening for incoming requests
+//     app.listen(port, hostname, () => {
+//         console.log(`Server started and accessible via http://${hostname}:${port}/`);
+//     });
+// })();
+
+    app.listen(port, () => {
+        console.log(`Server started and accessible via http://localhost:${port}/`);
+    });     
